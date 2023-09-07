4376bcf8391a55a91526337b21d5245c7c8e2b8f
Eslamhatab <mr.hatab055@gmail.com>
2023-09-07 19:21:19 +0300

    Success

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -65,9 +65,12 @@ class ProductController extends Controller
      * @param  \App\Models\Product  $product
      * @return \Illuminate\Http\Response
      */
-    public function edit(Product $product)
+    public function edit(int $id)
     {
         //
+        $Product = Product::find($id);
+        return view('pages.products.edit');
+
     }
 
     /**
@@ -77,10 +80,38 @@ class ProductController extends Controller
      * @param  \App\Models\Product  $product
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Product $product)
+    public function update(Request $request , int $id)
     {
-        //
-    }
+        //Validate
+            $request->validate([
+                'title'        => 'required|max255',
+                'price'        => 'nullabel|max255',
+                'description'  => 'nullable|max255'
+            ]);
+            //Update Products
+            $product_old = Product::find($id);
+            $product     = Product::find($id);
+
+            $product->title       =$request->title;
+            $product->price       =$request->price;
+            $product->description =$request->description;
+            $product->save();
+
+            if($product_old->title    != $product->title &&
+            $product_old->price       == $product->price &&
+            $product_old->description == $product->description){
+                $message_title ="Successful_product_title_updated";
+                $message_body  ="the product with Id($product->id)  Was Successfully update from \"$product_old->title\".";
+            }
+            elseif(($product_old->dsecription != $product->description &&
+            $product_old->title  == $product->title)){
+                $messagr_title ="Successfully_category_description_updated";
+                $message_body  ="the product  With ID ($product->id0) description was updated successfully."
+            }
+            else{
+                $message_title = "Successful_"
+            }
+        }
 
     /**
      * Remove the specified resource from storage.
